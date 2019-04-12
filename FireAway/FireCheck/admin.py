471ae9f1c35c89448223b95c4b3b94627712dda9
shubham2637 <shubham37@outlook.com>
2019-04-12 16:57:53 +0530

    database setup done

--- FireAway/FireCheck/admin.py
+++ FireAway/FireCheck/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(building)
+admin.site.register(zone)
+admin.site.register(panel)
+admin.site.register(device)
--- FireAway/FireCheck/models.py
+++ FireAway/FireCheck/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    username = models.CharField(max_length=20,unique=True)
+
+
+    def __str__(self):
+        return f"{self.id} {self.username}"
+
+class zone(models.Model):
+    name = models.CharField(max_length=128)
+    address = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    phone_no = models.IntegerField()
+    date_time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.id} {self.name}"
+
+class building(models.Model):
+    zone = models.ForeignKey(to=zone, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+
+    def __str__(self):
+        return f"{self.id} {self.name}"
+
+class panel(models.Model):
+    building = models.ForeignKey(to=building, on_delete=models.CASCADE)
+    location = models.TextField()
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"{self.id} {self.name}"
+
+class device(models.Model):
+    zone = models.ForeignKey(to=zone, on_delete=models.CASCADE)
+    building = models.ForeignKey(to=building,on_delete=models.CASCADE)
+    panel = models.ForeignKey(to=panel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    date_time_created = models.DateTimeField(auto_now_add=True)
+    last_checked = models.DateTimeField(auto_now=True)
+    health = models.CharField(max_length=1) #Y G R
+
+    def __str__(self):
+        return f"{self.id} {self.health} {self.zone.id} {self.building.id} {self.panel.id} "
