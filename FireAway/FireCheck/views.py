364ef72ff2623605fdd8b16df48e830eb6cdca46
shubham2637 <shubham37@outlook.com>
2019-04-13 10:14:23 +0530

    redisigned

--- FireAway/FireCheck/views.py
+++ FireAway/FireCheck/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.shortcuts import Http404,HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
-from .data import building_data
+#from .data import building_data
 from .models import *
 from django.contrib.auth.models import User,Group
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth import authenticate, login, logout
 def create(request):
-    building_data()
+    #building_data()
     context= {
         "devices" : device.objects.all()
     }
@@ -19,7 +19,7 @@ def index(request):
     context = {
         "buildings" : building.objects.all(),
         "devices" : device.objects.all(),
-        "td" : device.objects.all().count(),
+        "td" : building.objects.all().count(),
         "gd" : device.objects.filter(health='G').count(),
         "rd" : device.objects.filter(health='R').count(),
         "yd" : device.objects.filter(health='Y').count(),
@@ -70,9 +70,10 @@ def panel_details(request, pid):
 
 
 @login_required()
-def device_list(request):
+def device_list(request, pid):
+    b = panel.objects.get(pk=pid)
     context= {
-        "devices" : device.objects.all()
+        "devices" : device.objects.all().filter(panel=b)
     }
     return render(request, "FireCheck/table.html", context)
 
@@ -110,8 +111,9 @@ def zone_list(request):
     }
     return render(request, "FireCheck/zonelist.html", context)
 @login_required()
-def panel_list(request):
+def panel_list(request, bid):
+    b = building.objects.get(pk=bid)
     context= {
-        "panels" : panel.objects.all()
+        "panels" : panel.objects.all().filter(building=b)
     }
     return render(request, "FireCheck/panellist.html", context)
